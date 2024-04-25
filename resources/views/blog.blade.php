2caee106302a9d8d19b3c2b2edfd4302dfd1f6c3
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-04-25 20:29:35 +0700

    init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Welcome to Blog Page.</h1>
+    <article>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, id!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure itaque, dolorum sit hic doloribus similique dignissimos dolore labore at deleniti et libero assumenda dicta quibusdam sapiente accusamus ea deserunt sunt, saepe molestias soluta? Ad expedita culpa ex laudantium animi in molestiae impedit inventore incidunt obcaecati sit ipsam deserunt tenetur, dolor voluptas labore eveniet. Facere rem laudantium odio est fugit, impedit ipsa voluptates, corporis aperiam obcaecati perferendis tempore placeat, modi nostrum sapiente voluptatibus veniam? Fuga molestias laudantium exercitationem cum maxime nulla asperiores, excepturi praesentium a iure deserunt. Exercitationem, in. Cumque, ea! Eveniet cupiditate consectetur soluta voluptate tenetur rerum illo, in exercitationem.</p>
+    </article>
+    <article>
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, eaque.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolore iste minus dicta cupiditate distinctio sapiente perspiciatis nam impedit nesciunt? Corporis mollitia suscipit veritatis deserunt dicta, explicabo quibusdam libero fuga animi accusamus nulla quas fugit laudantium voluptate rerum reiciendis ad quidem aliquam sit consequuntur nostrum ullam perspiciatis? Repellat nihil omnis tempora quam officiis error beatae sequi inventore quae. Ea nemo placeat natus quaerat, distinctio eius aspernatur eligendi dolores alias nulla? Culpa cupiditate dignissimos temporibus. Sequi rem quos nesciunt iste exercitationem perferendis laborum explicabo nihil sunt autem, labore doloribus facere dolores inventore, quia mollitia voluptatem soluta voluptatum modi temporibus! Voluptates, esse.</p>
+    </article>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Welcome to Contact Page.</h1>
+    <h5>Name: {{ $name }}</h5>
+</body>
+
+</html>
